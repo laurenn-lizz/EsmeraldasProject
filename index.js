0725be175c93b3db1898d2f5a6c6e66711704323
Apple <apple@Macintosh.local>
2019-07-15 15:38:57 -0500

    trial

---
+++ index.js
@@ -0,0 +1,9 @@
+// function walkCricket() {
+//   console.log(Cricket need a walk NOW!);
+//   console.log("Bring a poop bag");
+//   console.log("Don't forget the leash");
+//
+// }
+
+var = donutHeader = document.querySelector('h1');
+ donutHeader.innerHTML = "Me Eat Donut Yum";
